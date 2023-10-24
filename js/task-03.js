9a66b6cea3dbc35536acfceb4ece40c814767979
il0naa <136170246+il0naa@users.noreply.github.com>
2023-10-24 13:06:46 +0200

    task-03

--- js/task-03.js
+++ js/task-03.js
@@ -12,3 +12,13 @@ const images = [
     alt: 'Group of Horses Running',
   },
 ];
+
+const gallery = document.querySelector(".gallery");
+const galleryHtml = images.map((image) => `
+  <li>
+    <img src="${image.url}" alt="${image.alt}">
+  </li>
+`).join('');
+
+
+gallery.insertAdjacentHTML('beforeend', galleryHtml);
