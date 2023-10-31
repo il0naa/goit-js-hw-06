189be0ccb1830181903466aaf8a76ac833c27400
il0naa <136170246+il0naa@users.noreply.github.com>
2023-10-31 11:08:56 +0100

    task 8

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,21 @@
+const loginForm = document.querySelector('.login-form');
 
+loginForm.addEventListener ('submit', function (event) {
+    event.preventDefault();
+
+    const emailInput = loginForm.elements.email;
+    const passwordInput = loginForm.elements.password;
+
+    if(!emailInput.value || !passwordInput.value) {
+        alert('Все поля должны быть заполнены');
+    } else {
+        const formData = {
+            email: emailInput.value,
+            password: passwordInput.value,
+        }
+
+        console.log(formData);
+        loginForm.reset();
+    };
+
+});
